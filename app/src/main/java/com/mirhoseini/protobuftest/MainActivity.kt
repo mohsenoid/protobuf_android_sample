9d615f5ed67a2dc36265337e1963b73f59e67f51
Mohsen <mohsen@germanautolabs.com>
2017-12-23 17:47:49 +0100

    Add comparison with JSON

--- app/src/main/java/com/mirhoseini/protobuftest/MainActivity.kt
+++ app/src/main/java/com/mirhoseini/protobuftest/MainActivity.kt
@@ -9,22 +9,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bytes = createAddressBook()
+        val bytes = testAddressBookProtobuf()
 
-        println(AddressBookProtos.AddressBook.parseFrom(bytes))
+        // You can deserialize AddressBook bytes
+        val myAddressBook = AddressBookProtos.AddressBook.parseFrom(bytes)
+        println(myAddressBook)
+
+        println("Protobuf byte size: ${bytes.size}")
+        println("JSON byte size: ${sampleJson.toByteArray().size}")
     }
 
-    private fun createAddressBook(): ByteArray? {
+    private fun testAddressBookProtobuf(): ByteArray {
+        // building PhoneNumber objects
         val phoneHome = AddressBookProtos.Person.PhoneNumber.newBuilder()
                 .setNumber("123456")
                 .setType(AddressBookProtos.Person.PhoneType.HOME)
                 .build()
-
         val phoneMobile = AddressBookProtos.Person.PhoneNumber.newBuilder()
                 .setNumber("654321")
                 .setType(AddressBookProtos.Person.PhoneType.MOBILE)
                 .build()
 
+        // building a Person object using phone data
         val person = AddressBookProtos.Person.newBuilder()
                 .setId(1)
                 .setName("Mohsen")
@@ -32,10 +38,41 @@ class MainActivity : AppCompatActivity() {
                 .addAllPhones(listOf(phoneHome, phoneMobile))
                 .build()
 
+        // building an AddressBook object using person data
         val addressBook = AddressBookProtos.AddressBook.newBuilder()
                 .addAllPeople(listOf(person))
                 .build()
 
+        // finally this is how you get serialized ByteArray
         return addressBook.toByteArray()
     }
+
+    private val sampleJson = """{
+  "addressbook": [
+    {
+      "person": {
+        "id": 1,
+        "name": "Mohsen",
+        "email": "mohsen@mirhoseini.info"
+      }
+    },
+    {
+      "phones": [
+        {
+          "phone": {
+            "number": "123456",
+            "type": "HOME"
+          }
+        },
+        {
+          "phone": {
+            "number": "654321",
+            "type": "MOBILE"
+          }
+        }
+      ]
+    }
+  ]
+}"""
 }
+
